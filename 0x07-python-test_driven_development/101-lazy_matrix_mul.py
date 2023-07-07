3d220834f75506a91188360586a5ccd4d46fed1a
Abdesslam Mekouar <a.mekouar@outlook.com>
2023-07-07 04:29:46 -0700

    push Lazy matrix multiplication

---
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""module lazy_matrix_mul Matrix multiplication using NumPy."""
+
+
+import Numpy
+
+
+def lazy_matrix_mul(m_a, m_b):
+    """multiplies m_a and m_b using matmul, and returns the result."""
+
+    return numpy.matmul(m_a, m_b)
